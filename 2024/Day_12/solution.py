02f5a843bcf0762c6befc143d8861e09190e8e8d
JessyColonval <jessy.colonval@gmail.com>
2025-09-23 11:48:45 +0200

    Advent of code 2024, Day 12 solution (Python) | Comments

--- 2024/Day_12/solution.py
+++ 2024/Day_12/solution.py
@@ -1,91 +1,198 @@
 """
 Advent of code - Day 12 (2024)
 """
+from typing import List, Tuple, Dict
+
 import sys
 import time
 
 
-def extract_regions(garden):
+def extract_regions(matrix: List[List[str]]) -> List[List[Tuple[int]]]:
+    """
+    Gives each different regions in the garden seperatly.
+
+    Parameters
+    ----------
+    matrix : List[List[str]]
+        Matrix that represents the garden.
+
+    Result
+    ------
+    List[List[Tuple[int]]]
+        A list for each different regions that contains the coordinates of
+        plants presents in it.
+    """
+    # Matrix that indicates if a plant was already visited.
     visited = [
-        [False for j in range(0, len(garden[i]))]
-        for i in range(0, len(garden))
+        [False for j in range(0, len(matrix[i]))]
+        for i in range(0, len(matrix))
     ]
-    regions = []
 
-    for i, line in enumerate(garden):
-        for j, plant in enumerate(line):
+    result = []
+    for i, line in enumerate(matrix):
+        for j in range(len(line)):
             if not visited[i][j]:
+                # If the current plant wasn't already visited then that means
+                # this is a new area of plant.
                 region = [(i, j)]
+
+                # Set the current has visited.
                 visited[i][j] = True
-                recursion(garden, plant, i, j, visited, region)
-                regions.append(region)
 
-    return regions
+                # Find every adjacent similar plants until no new plant is
+                # found.
+                recursion(matrix, i, j, visited, region)
+
+                # Append the region in the result.
+                result.append(region)
+
+    return result
 
 
-def recursion(garden, plant, i, j, visited, indices):
+def recursion(
+        matrix: List[List[str]],
+        i: int,
+        j: int,
+        visited: List[List[bool]],
+        indices: List[Tuple[int]]
+) -> None:
+    """
+    Find recursively every similar plant in one region, i.e. adjacent to at
+    least one plant to the region.
+
+    Parameters
+    ----------
+    matrix: List[List[str]]
+        Matrix that represents the garden.
+    i: int
+        Coordinate x of the current plant.
+    j: int
+        Coordinate y of the currant plant.
+    visited: List[List[bool]]
+        Matrix of already visited plant in the garden.
+    indices: List[Tuple[int]]
+        A list of coordinates of similar plants in a region.
+    """
+    # Name of the current plant.
+    plant = matrix[i][j]
+
     # Above.
     ab = i-1
-    if (ab >= 0 and garden[ab][j] == plant and not visited[ab][j]):
+    if (ab >= 0 and matrix[ab][j] == plant and not visited[ab][j]):
         visited[ab][j] = True
         indices.append((ab, j))
-        recursion(garden, plant, ab, j, visited, indices)
+        recursion(matrix, ab, j, visited, indices)
 
     # Below.
     be = i+1
-    if (be < len(garden) and garden[be][j] == plant and not visited[be][j]):
+    if (be < len(matrix) and matrix[be][j] == plant and not visited[be][j]):
         visited[be][j] = True
         indices.append((be, j))
-        recursion(garden, plant, be, j, visited, indices)
+        recursion(matrix, be, j, visited, indices)
 
     # Right.
     ri = j+1
-    if (ri < len(garden[i]) and garden[i][ri] == plant and not visited[i][ri]):
+    if (ri < len(matrix[i]) and matrix[i][ri] == plant and not visited[i][ri]):
         visited[i][ri] = True
         indices.append((i, ri))
-        recursion(garden, plant, i, ri, visited, indices)
+        recursion(matrix, i, ri, visited, indices)
 
     # Left.
     le = j-1
-    if (le >= 0 and garden[i][le] == plant and not visited[i][le]):
+    if (le >= 0 and matrix[i][le] == plant and not visited[i][le]):
         visited[i][le] = True
         indices.append((i, le))
-        recursion(garden, plant, i, le, visited, indices)
+        recursion(matrix, i, le, visited, indices)
 
 
-def plant_perimeter(garden: list, i: int, j: int) -> int:
-    plant = garden[i][j]
+def plant_perimeter(matrix: List[List[str]], i: int, j: int) -> int:
+    """
+    Computes the perimeter of one plant.
+
+    Parameters
+    ----------
+    matrix : List[List[str]]
+        Matrix that represents the garden.
+    i : int
+        Coordinate x of the plant.
+    j : int
+        Coordinate y of the plant.
+
+    Return
+    ------
+    int
+        The perimeter of the plant at (i, j).
+    """
+    plant = matrix[i][j]  # Name of the plant at (i, j).
+    n_lines = len(matrix)  # Number of lines
+    n_cols = len(matrix[i])  # Number of columns.
+
+    # Sets the result at zero.
     result = 0
 
     # Check above the plant.
-    if i-1 < 0 or garden[i-1][j] != plant:
+    ab = i-1
+    if ab < 0 or matrix[ab][j] != plant:
         result += 1
 
     # Check below the plant.
-    if i+1 >= len(garden) or garden[i+1][j] != plant:
+    be = i+1
+    if be >= n_lines or matrix[be][j] != plant:
         result += 1
 
     # Check at the right of the plant.
-    if j+1 >= len(garden[i]) or garden[i][j+1] != plant:
+    ri = j+1
+    if ri >= n_cols or matrix[i][ri] != plant:
         result += 1
 
     # Check at the left of the plant.
-    if j-1 < 0 or garden[i][j-1] != plant:
+    le = j-1
+    if le < 0 or matrix[i][le] != plant:
         result += 1
 
     return result
 
 
-def region_perimeter(garden, region):
+def region_perimeter(matrix: List[List[str]],
+                     indices: List[Tuple[int]]) -> int:
+    """
+    Computes the perimeter of a region.
+
+    Parameters
+    ----------
+    matrix : List[List[str]]
+        Matrix that represents the garden.
+    indices: List[Tuple[int]]
+        A list of coordinates of similar plants that shares the same region.
+
+    Return
+    ------
+    int
+        The perimeter of the given region.
+    """
     return sum(
-        plant_perimeter(garden, coords[0], coords[1])
-        for coords in region
+        plant_perimeter(matrix, x, y)
+        for x, y in indices
     )
 
 
-def divide_by_line(region):
+def divide_by_line(indices: List[Tuple[int]]) -> Dict[int, List[Tuple[int]]]:
+    """
+    Separates a region according to its lines.
+
+    Parameters
+    ----------
+    indices : List[Tuple[int]]
+        A list of coordinates of similar plants that shares the same region.
+
+    Return
+    ------
+    Dict[int, List[Tuple[int]]]
+        A dictionnary that map a number of line in the garden with the
+        coordinates of every plant in the given region on this line.
+    """
     result = {}
-    for plant in region:
+    for plant in indices:
         x = plant[0]
         if x not in result:
             result[x] = []
@@ -96,9 +203,23 @@ def divide_by_line(region):
     }
 
 
-def divide_by_column(region):
+def divide_by_column(indices: List[Tuple[int]]) -> Dict[int, List[Tuple[int]]]:
+    """
+    Separates a region according to its columns.
+
+    Parameters
+    ----------
+    indices : List[Tuple[int]]
+        A list of coordinates of similar plants that shares the same region.
+
+    Return
+    ------
+    Dict[int, List[Tuple[int]]]
+        A dictionnary that map a number of column in the garden with the
+        coordinates of every plant in the given region on this column.
+    """
     result = {}
-    for plant in region:
+    for plant in indices:
         y = plant[1]
         if y not in result:
             result[y] = []
@@ -109,7 +230,20 @@ def divide_by_column(region):
     }
 
 
-def count_sequence_of_true(sequence) -> int:
+def count_sequence_of_true(sequence: List[bool]) -> int:
+    """
+    Count the number of sequence of 'True' in a given list.
+
+    Parameters
+    ----------
+    sequence: List[bool]
+        A list of bool.
+
+    Return
+    ------
+    int
+        The number of different sequence of 'True' in the given list.
+    """
     count = 0
     in_sequence = False
     for e in sequence:
@@ -122,16 +256,35 @@ def count_sequence_of_true(sequence) -> int:
     return count
 
 
-def number_of_h_faces(garden, region_by_lines) -> int:
-    N_LINES = len(garden)
+def number_of_h_faces(
+    matrix: List[List[str]],
+    region_by_lines: Dict[int, List[Tuple[int]]]
+) -> int:
+    """
+    Computes the number of horizontal faces present for a given region.
+
+    Parameters
+    ----------
+    matrix : List[List[str]]
+        Matrix that represents the garden.
+    region_by_lines : Dict[int, List[Tuple[int]]]
+        Dictionary that links the coordinates of plants present in the region
+        to each row number in the garden.
+
+    Return
+    ------
+    int
+        Number of horizontal faces.
+    """
+    n_lines = len(matrix)
     result = 0
     for line in region_by_lines.values():
-        top = [False for _ in range(N_LINES)]
-        bottom = [False for _ in range(N_LINES)]
+        top = [False for _ in range(n_lines)]
+        bottom = [False for _ in range(n_lines)]
         for x, y in line:
-            if x-1 < 0 or garden[x-1][y] != garden[x][y]:
+            if x-1 < 0 or matrix[x-1][y] != matrix[x][y]:
                 top[y] = True
-            if x+1 >= N_LINES or garden[x+1][y] != garden[x][y]:
+            if x+1 >= n_lines or matrix[x+1][y] != matrix[x][y]:
                 bottom[y] = True
 
         result += count_sequence_of_true(top)
@@ -140,16 +293,35 @@ def number_of_h_faces(garden, region_by_lines) -> int:
     return result
 
 
-def number_of_v_faces(garden, region_by_columns) -> int:
-    N_COLUMNS = len(garden[0])
+def number_of_v_faces(
+    matrix: List[List[str]],
+    region_by_cols: Dict[int, List[Tuple[int]]]
+) -> int:
+    """
+    Computes the number of vertical faces present for a given region.
+
+    Parameters
+    ----------
+    matrix : List[List[str]]
+        Matrix that represents the garden.
+    region_by_cols : Dict[int, List[Tuple[int]]]
+        Dictionary that links the coordinates of plants present in the region
+        to each column number in the garden.
+
+    Return
+    ------
+    int
+        Number of vertical faces.
+    """
+    n_cols = len(matrix[0])
     result = 0
-    for column in region_by_columns.values():
-        left = [False for _ in range(0, N_COLUMNS)]
-        right = [False for _ in range(0, N_COLUMNS)]
+    for column in region_by_cols.values():
+        left = [False for _ in range(0, n_cols)]
+        right = [False for _ in range(0, n_cols)]
         for x, y in column:
-            if y-1 < 0 or garden[x][y-1] != garden[x][y]:
+            if y-1 < 0 or matrix[x][y-1] != matrix[x][y]:
                 left[x] = True
-            if y+1 >= N_COLUMNS or garden[x][y+1] != garden[x][y]:
+            if y+1 >= n_cols or matrix[x][y+1] != matrix[x][y]:
                 right[x] = True
 
         result += count_sequence_of_true(left)
@@ -158,16 +330,29 @@ def number_of_v_faces(garden, region_by_columns) -> int:
     return result
 
 
-def number_of_faces(garden, region) -> int:
-    if (len(region) == 1
-       or all(plant[0] == region[0][0] for plant in region)
-       or all(plant[1] == garden[0][1] for plant in region)):
+def number_of_faces(matrix: List[List[str]], indices: List[Tuple[int]]) -> int:
+    """
+    Parameters
+    ----------
+    matrix : List[List[str]]
+        Matrix that represents the garden.
+    indices : List[Tuple[int]]
+        A list of coordinates of similar plants that shares the same region.
+
+    Return
+    ------
+    int
+        Number of faces.
+    """
+    if (len(indices) == 1
+       or all(x == indices[0][0] for x, _ in indices)
+       or all(y == indices[0][1] for _, y in indices)):
         return 4
 
-    region_by_lines = divide_by_line(region)
-    region_by_columns = divide_by_column(region)
-    return number_of_h_faces(garden, region_by_lines) + \
-        number_of_v_faces(garden, region_by_columns)
+    i_by_lines = divide_by_line(indices)
+    i_by_columns = divide_by_column(indices)
+    return number_of_h_faces(matrix, i_by_lines) + \
+        number_of_v_faces(matrix, i_by_columns)
 
 
 if __name__ == "__main__":
